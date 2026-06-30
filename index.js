5d483a773db9da78e56c47be5affdf00b0ef9c5a
Akashdeep Singh <akashmansa58@gmail.com>
2026-06-30 19:41:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const multer = require("multer");
+
+
+const app = express();
+const PORT = 8000;
+
+// const upload = multer({ dest: "uploads/"}); //Save file into uploads folder
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        return cb(null,"./uploads");
+    },
+    filename: function(req,file, cb) {
+        return cb(null,`${Date.now()}-${file.originalname}`);
+    },  // To remove clash
+})
+
+const upload = multer({ storage});
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.urlencoded( { extended: false })); // It helps in parsing our Form data
+
+app.get("/",(req, res) => {
+    return res.render("homepage");
+});
+
+app.post('/upload', upload.single("profileImage"), (req,res) => {
+    console.log(req.body);
+    console.log(req.file);
+    
+   return res.redirect('/'); 
+});
+
+app.listen(PORT, () => console.log(`Server started at PORT:8000`));
